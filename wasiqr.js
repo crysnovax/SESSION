18a51cf3587c1e55703271bdf1261f6ebbf501ab
CRYSTAL-LEVI UZOUKWU <carayasata1la@gmail.com>
2026-02-13 15:44:40 +0100

    Update wasiqr.js

--- wasiqr.js
+++ wasiqr.js
@@ -59,21 +59,21 @@ router.get('/', async (req, res) => {
 				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
 	
 				   let WASI_MD_TEXT = `
-*_Session Connected By Wasi Tech_*
-*_Made With 🤍_*
+*_Session Connected By crysnova x*
+*_Made With 🤍☠️_*
 ______________________________________
 ╔════◇
-║ *『AMAZING YOU'VE CHOSEN WASI MD』*
+║ *『AMAZING YOU'VE CHOSEN CRYSNOVA AI』*
 ║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@wasitech1
-║❒ *Owner:* _https://wa.me/message/THZ3I25BYZM2E1_
-║❒ *Repo:* _https://github.com/wasixd/WASI-MD_
-║❒ *WaGroup:* _https://chat.whatsapp.com/FF6YuOZTAVB6Lu65cnY5BN_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j_
-║❒ *Plugins:* _https://github.com/Itxxwasi 
+║❒ *Ytube:* _youtube.com/@crysnovax_
+║❒ *Owner:* _https://wa.me/2348077528901_
+║❒ *Repo:* _https://github.com/crysnovax/CRYSNOVA-AI_
+║❒ *WaGroup:* _https://chat.whatsapp.com/Besbj8VIle1GwxKKZv1lax?mode=gi_t_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029Vb6pe77K0IBn48HLKb38_
+║❒ *Plugins:* _https://github.com/crysnovax
 ╚════════════════════════╝
 _____________________________________
 	
